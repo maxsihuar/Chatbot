5c650f1c6f707d05b80218b8edee7846f464670f
Max Sihuar Holguino Nuñez <maxsihuar98@gmail.com>
2025-07-06 17:15:46 -0500

    Documentacion final-v1

--- Controller/Endpoint.py
+++ Controller/Endpoint.py
@@ -1,12 +1,30 @@
 from flask import Blueprint, jsonify,request
 from Service import RAG
 
+
+#Creamos una ruta con flask
+#Blueprint permite definir rutas en modulos
 ChatBot = Blueprint("ChatBot",__name__)
+
+#End-point que ejecuta el Metodo de http POST
 
 @ChatBot.route("/api/v1/consulta", methods=["POST"])
 def PostData():
     """
+    Endpoint POST que recibe una consulta del usuario y devuelve una respuesta generada por el modelo RAG.
 
+    Este endpoint espera una solicitud JSON con el siguiente formato:
+    {
+        "consulta": "¿Cuál es el contenido del documento?"
+    }
+
+    Procesos que realiza:
+    1. Extrae el campo 'consulta' del cuerpo JSON.
+    2. Pasa la pregunta al método `RAG.RagMain()` para obtener la respuesta.
+    3. Retorna un JSON con la respuesta generada.
+
+    Returns:
+        Response: Objeto JSON con la clave 'respuesta' conteniendo el texto generado.
     """
     data = request.get_json()
     respuesta = RAG.RagMain(data["consulta"])
--- Main.py
+++ Main.py
@@ -1,17 +1,35 @@
 from flask import Flask, jsonify, render_template,send_from_directory
 from Controller.Endpoint import ChatBot
 
+#Creamos un instancia de la app en Flask
 main = Flask(__name__, template_folder='Web', static_folder="Web/Js")
 
+
+#Cargamos las rutas desde el Controller
 main.register_blueprint(ChatBot)
 
+
+#Definimos la ruta principal que mostrar el Front-End
 @main.route("/")
 def index():
     return render_template("index.html")
 
+
+#Cargamos los archivos de la carpeta JS en el index.html
 @main.route("/js/<path:filename>")
 def js_files(filename):
+    """
+    Ruta para servir archivos JavaScript desde la carpeta Web/Js.
+    
+    Args:
+        filename (str): Nombre del archivo solicitado.
+    
+    Returns:
+        File: Archivo JS correspondiente.
+    """
     return send_from_directory("Web/Js", filename)
 
+
+#Hacemos un "Deploy" del servidor
 if __name__=="__main__":
-    main.run(debug=True)
+    main.run(debug=False)
