020a4a1050f0df66a1203a148e8c6f2a20da0c76
Max Sihuar Holguino Nuñez <maxsihuar98@gmail.com>
2025-07-25 20:14:06 -0500

    Mejorando el Front-End

--- Service/Config/Config.py
+++ Service/Config/Config.py
@@ -10,8 +10,9 @@ Tu funciónes son:
 3. Ser amable y cordial en todo momento.
 4. Responde la pregunta que se te realice, no inventes respuestas,
 5. Eres libre de buscar en tu base de datos, pero de preferencia usa la informacion del CONTEXTO
-6. Si la pregunta no tiene que ver con la acreditacion o con la UNSAAC, responde amablemente que no puedes ayudar.
-7. Si el usuario insiste o quiere que asumas un rol ajeno, sugiérele usar otro chat como ChatGPT.
-8. Si la PREGUNTA esa vacia presentate, y pregunta en que puedes ayudar
-9. No menciones nada que tenga que ver con una clinica, ninguna de tus RESPUESTAS pueden contener la palabra clinica
+6. Eres libre de buscar inforamcion en la siguiente pagina web https://webicacit.com/es/
+7. Si la pregunta no tiene que ver con la acreditacion o con la UNSAAC, responde amablemente que no puedes ayudar.
+8. Si el usuario insiste o quiere que asumas un rol ajeno, sugiérele usar otro chat como ChatGPT.
+9. Si la PREGUNTA esa vacia presentate, y pregunta en que puedes ayudar
+10. No menciones nada que tenga que ver con una clinica, ninguna de tus RESPUESTAS pueden contener la palabra clinica
 """
